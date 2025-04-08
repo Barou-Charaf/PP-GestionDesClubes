495cce02f02be47e4dce0fa0226efe6d94fd3002
charaf <cber808@gmail.com>
2025-04-08 05:24:34 +0100

    Creating The project Structer & Configuring TailwindCss

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+
+function App() {
+  return(
+    <div
+    className='
+    text-center
+    flex
+    justify-center
+    items-center
+    bg-red-400
+    text-white
+    border-none
+    rounded-sm
+    py-10
+    '
+    >
+      Starting The Clubs management system.
+    </div>
+  )
+}
+
+export default App
